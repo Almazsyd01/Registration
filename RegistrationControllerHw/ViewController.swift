2627a0fa443541d0f83596c6e85ba2afa29d962e
almaz <almazsyd5@gmail.com>
2024-03-21 23:11:59 +0600

    Initial Commit

---
+++ RegistrationControllerHw/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RegistrationControllerHw
+//
+//  Created by Mac on 21/3/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
